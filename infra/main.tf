fa7b57389d7272487bc907401e09dcfdad78c2ed
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-10 00:39:25 -0300

    wip: remove key pair config

--- infra/main.tf
+++ infra/main.tf
@@ -31,7 +31,6 @@ resource "aws_security_group" "securitygroup" {
 resource "aws_instance" "ec2" {
   ami                    = "ami-0c101f26f147fa7fd"
   instance_type          = "t2.nano"
-  key_name               = aws_key_pair.keypair.key_name
   user_data              = file("user_data.sh")
   vpc_security_group_ids = ["${aws_security_group.securitygroup.id}"]
 }
