55c0c4811214970adfee8c1a97139f8d3c850dac
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-10 01:44:29 -0300

    wip: vpc_id

--- infra/main.tf
+++ infra/main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "ec2" {
   ami                    = "ami-0c101f26f147fa7fd"
   instance_type          = "t2.nano"
   user_data              = file("user_data.sh")
-  vpc_security_group_ids = ["${aws_security_group.securitygroup.id}"]
+  vpc_security_group_ids = ["vpc-0b08aa7501e0bd2c7"]
 }
 
 output "public_ip" {
