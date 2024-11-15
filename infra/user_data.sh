7554f0edbb01b43134e6417c14f96f7e5dfbfca7
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-15 19:30:33 -0300

    fix: conver user_data to unix and comment infra creation

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+chmod +x /path/to/your/script.sh
 sudo su
 yum update -y
 yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
-
 docker run -p 80:5000 ninjadexter/triagem_facil_web:latest
