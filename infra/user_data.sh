9736166cd9b160746c08ac4ca7f69e1a9e2b054a
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-15 19:05:36 -0300

    fix: repository docker name

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -6,4 +6,4 @@ yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
 
-docker run -p 80:5000 hackathon/triagem_facil_web:latest
+docker run -p 80:5000 ninjadexter/triagem_facil_web:latest
