2b71024bb51140fab646d7eb1add4f7ac5ce6587
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-15 19:44:04 -0300

    fix: Dockerfile app script and user_data docker login

--- Dockerfile
+++ Dockerfile
@@ -19,4 +19,4 @@ RUN pip install gunicorn
 
 EXPOSE 5000
 
-CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:server"]
+CMD ["gunicorn", "-b", "0.0.0.0:5000", "app:app"]
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -5,4 +5,5 @@ yum update -y
 yum install -y docker
 service docker start
 usermod -a -G docker ec2-user
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker run -p 80:5000 ninjadexter/triagem_facil_web:latest
