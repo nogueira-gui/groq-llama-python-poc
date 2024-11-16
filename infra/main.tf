239f163a4e8c16b74c0c2d24056517e72befd8f3
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-16 00:44:15 -0300

    wip: create ec2

--- infra/main.tf
+++ infra/main.tf
@@ -20,18 +20,18 @@ provider "aws" {
 #   })
 # }
 
-# Política gerenciada do SSM Parameter Store anexada à Role
-resource "aws_iam_policy_attachment" "ssm_policy" {
-  name       = "ssm-policy-attachment"
-  roles      = ["ec2-ssm-role"]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
-}
+# # Política gerenciada do SSM Parameter Store anexada à Role
+# resource "aws_iam_policy_attachment" "ssm_policy" {
+#   name       = "ssm-policy-attachment"
+#   roles      = ["ec2-ssm-role"]
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+# }
 
-# Perfil de instância para anexar a Role à EC2
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "ec2-instance-profile"
-  role = "ec2-ssm-role"
-}
+# # Perfil de instância para anexar a Role à EC2
+# resource "aws_iam_instance_profile" "ec2_instance_profile" {
+#   name = "ec2-instance-profile"
+#   role = "ec2-ssm-role"
+# }
 
 data "aws_security_group" "existing_sg" {
   filter {
