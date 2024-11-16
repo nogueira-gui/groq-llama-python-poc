11d2474961954984c2a1ef5392b318cc6f552a00
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-16 00:32:41 -0300

    infra: security group id

--- infra/main.tf
+++ infra/main.tf
@@ -70,7 +70,7 @@ resource "aws_instance" "ec2" {
   instance_type          = "t2.micro"
   user_data              = file("user_data.sh")
   iam_instance_profile = "ec2-instance-profile"
-  vpc_security_group_ids = [data.existing_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 }
 
 output "public_ip" {
