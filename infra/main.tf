fb99b9a8ff35b548a7b3a91aae8f93416df95004
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-16 00:25:11 -0300

    wip: destroy

--- infra/main.tf
+++ infra/main.tf
@@ -4,72 +4,72 @@ provider "aws" {
 
 
 # Role para a instância EC2
-# resource "aws_iam_role" "ec2_ssm_role" {
-#   name               = "ec2-ssm-role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect    = "Allow",
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         },
-#         Action = "sts:AssumeRole"
-#       }
-#     ]
-#   })
-# }
+resource "aws_iam_role" "ec2_ssm_role" {
+  name               = "ec2-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-# # Política gerenciada do SSM Parameter Store anexada à Role
-# resource "aws_iam_policy_attachment" "ssm_policy" {
-#   name       = "ssm-policy-attachment"
-#   roles      = [aws_iam_role.ec2_ssm_role.name]
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
-# }
+# Política gerenciada do SSM Parameter Store anexada à Role
+resource "aws_iam_policy_attachment" "ssm_policy" {
+  name       = "ssm-policy-attachment"
+  roles      = [aws_iam_role.ec2_ssm_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
 
-# # Perfil de instância para anexar a Role à EC2
-# resource "aws_iam_instance_profile" "ec2_instance_profile" {
-#   name = "ec2-instance-profile"
-#   role = aws_iam_role.ec2_ssm_role.name
-# }
+# Perfil de instância para anexar a Role à EC2
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_ssm_role.name
+}
 
-data "aws_security_group" "existing_sg" {
-  filter {
-    name   = "group-name"
-    values = ["ec2-securitygroup"]
+# data "aws_security_group" "existing_sg" {
+#   filter {
+#     name   = "group-name"
+#     values = ["ec2-securitygroup"]
+#   }
+# }
+resource "aws_security_group" "securitygroup" {
+  name        = "ec2-securitygroup"
+  description = "Ingress Http and SSH and Egress to anywhere"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-# resource "aws_security_group" "securitygroup" {
-#   name        = "ec2-securitygroup"
-#   description = "Ingress Http and SSH and Egress to anywhere"
-
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
 
 resource "aws_instance" "ec2" {
   ami                    = "ami-012967cc5a8c9f891"
   instance_type          = "t2.micro"
   user_data              = file("user_data.sh")
-  iam_instance_profile    = "ec2-ssm-role"
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
 }
 
