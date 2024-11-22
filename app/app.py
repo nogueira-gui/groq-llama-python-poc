2768bea37e1384b88c8d836ab4bec7ce8591ab78
GUILHERME N SILVA <guilherme.nogueiras@outlook.com>
2024-11-22 14:53:04 -0300

    refactor: remove headers log

--- app/app.py
+++ app/app.py
@@ -43,7 +43,6 @@ eb6a05ae2173450'''
 def require_api_key(func):
     def wrapper(*args, **kwargs):
         auth = request.headers.get('Authorization')
-        logging.info(f"headers: {request.headers}")
         if auth and auth == API_KEY:
             logging.info("API key válida recebida")
             return func(*args, **kwargs)
